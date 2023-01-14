6f60883e7ef8326b12f2da5be9226cdf14032625
kunal <kunaldayal.786.kd@gmail.com>
2023-01-14 11:41:18 +0530

    1st try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new product();
+    }
+class Product{
 
+    }
+
+    public int product(int x, int y) {
+
+    }
+    public int product(int x, int y, int z) {
+
+    }
+    public double product(double x, double y) {
+
+    }
 }
